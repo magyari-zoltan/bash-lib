bf5bc87ab0937616bcf34e8e8558df4761df7721
Magyari Zoltán <codesage.throttle642@passmail.net>
2026-06-09 06:16:37 +0300

    Debug tool

---
+++ lib/debug.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# Debug helper library
+#
+# This file provides small helper functions for controlling shell trace mode 
+# `set -x` in Bash scripts. When trace mode is enabled, Bash prints each command 
+# before executing it, which is useful for debugging script flow.
+#
+# The `DEBUG` variable controls whether tracing should be enabled:
+# 0 - tracing disabled
+# 1 - tracing enabled
+# ------------------------------------------------------------------------------
+
+# Prevent multiple sourcing
+[[ -v "DEBUG_LOADED" ]] && return 0 # Return instead of exit to avoid terminating the calling script.
+readonly DEBUG_LOADED=true
+
+# ------------------------------------------------------------------------------
+# Public API: Functions intended for external use
+# ------------------------------------------------------------------------------
+
+# Global trace mode swith
+# 0 - disabled
+# 1 - enabled
+DEBUG=1
+
+# Enables Bash trace mode when debugging is active.
+function debug_on {
+	if [[ "$DEBUG" != "0" ]]; then
+		echo "+ debug_on" >&2
+		set -x # trace mode on: echo executed commands.
+	fi
+}
+
+# Disables Bash trace mode when debugging is active.
+function debug_off() {
+	{ 
+		if [[ "$DEBUG" != "0" ]]; then
+			set +x; # trace mode off: don't echo executed commands.
+		fi
+	} 2> /dev/null # Prevents commands in thist block to be traced.
+}
+
+# ------------------------------------------------------------------------------
+
---
+++ test/debug.test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Get current scripts absolute path
+CURRENT_SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# The "lib" folders relative path
+LIB="$CURRENT_SCRIPT_PATH/../lib"
+
+
+# Import library scripts
+source "$LIB/unit_test.sh"
+source "$LIB/debug.sh"
+
+# ------------------------------------------------------------------------------
+# Cleanup
+# ------------------------------------------------------------------------------
+
+function cleanup() {
+	cleanup_test_env
+}
+
+trap 'cleanup' EXIT
+
+# ------------------------------------------------------------------------------
+# Test cases
+# ------------------------------------------------------------------------------
+
+DESCRIBE "Turning debug on will print the commands on the error output"
+
+function turn_debug_on_and_off() {
+	debug_on
+	echo "Hello World!"
+	debug_off
+	echo "Whats up!"
+
+}
+
+RUN turn_debug_on_and_off
+
+expected="Hello World!
+Whats up!"
+copy_stdout_to output
+
+EXPECT_TO_BE_EQUAL "$expected" "$output" "The output is not the expected one:" 
+[[ $? -ne 0 ]] && echo "$output"
+
+ENDTEST
+
+# ==============================================================================
+
+DESCRIBE "Disable debug entierly"
+
+function disable_debug_entierly() {
+	DEBUG=0
+	debug_on
+	echo "Debug is globally disabled"
+	debug_off
+	DEBUG=1
+}
+
+RUN disable_debug_entierly
+
+expected="Debug is globally disabled"
+copy_stdout_to output
+
+EXPECT_TO_BE_EQUAL "$expected" "$output" "The output is not the expected one:" 
+[[ $? -ne 0 ]] && echo "$output"
+
+ENDTEST
+
+# ==============================================================================
